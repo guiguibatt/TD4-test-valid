929c71d5ef996e43af442cb9350e62e6f2bad0b1
Andy Houillon <andy.houillon@etu.umontpellier.fr>
2020-02-26 11:25:31 +0100

    Basic test Good

--- test/RacineTest.java
+++ test/RacineTest.java
@@ -13,7 +13,11 @@ public class RacineTest {
     }
 
     @Test
-    public void istrue() {
-        assertEquals(true, true);
+    public void calculer_2_4_should_give_an_array_with_4_9_16() {
+        int[] tab = new int[3];
+        tab[0] = 4;
+        tab[1] = 9;
+        tab[2] = 16;
+        assertArrayEquals(Racine.calculer(2,4), tab);
     }
 }
