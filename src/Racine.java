58cbb4b8d62e3651aab264d5e996b23b75b18ba3
Guilhem Agussol <guilhem.agussol@etu.umontpellier.fr>
2020-02-26 11:23:16 +0100

    racine up to test

--- src/Racine.java
+++ src/Racine.java
@@ -1,6 +1,24 @@
 public class Racine {
 
-    private int a ;
-    private int b;
+    public static int[] calculer(int a,int b){
+        int j = b-a+1;
+        int[] lst = new int[j];
 
+        for (int i = 0; i < j; i++) {
+
+            lst[i]= (a+i)*(a+i);
+
+        }
+    return lst;
+    }
+
+    public static void main(String[] args) {
+        int[] tab = calculer(2,4);
+        for (int i = 0; i <= tab.length-1; i++) {
+            System.out.println(tab[i]);
+        }
+        System.out.println();
+
+
+    }
 }
