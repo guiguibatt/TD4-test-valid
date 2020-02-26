1e81e1f06de98379aa9d77f2adc97d7867d52235
Guilhem Agussol <guilhem.agussol@etu.umontpellier.fr>
2020-02-26 10:54:01 +0100

    création fichier

---
+++ test/RacineTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RacineTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void istrue() {
+        assertEquals(true, true);
+    }
+}
