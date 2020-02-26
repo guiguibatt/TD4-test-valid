0c21dc33c8d4704b0b4472e94afe6ba575b4faff
Andy Houillon <andy.houillon@etu.umontpellier.fr>
2020-02-26 12:14:24 +0100

    Triangle usable

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,8 @@
 public class Triangle {
+    public static int calculer(int a, int b, int c) {
+        if (a <=0 || b<=0 || c<= 0) return 4;
+        if ( a == b && b == c) return 1;
+        if (a == c || b ==c) return 2;
+        else return 3;
+    }
 }
