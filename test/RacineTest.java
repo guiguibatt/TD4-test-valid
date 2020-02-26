54dddd7d32cb2eff8a432a0bfa9e11980200af4d
Andy Houillon <andy.houillon@etu.umontpellier.fr>
2020-02-26 11:46:56 +0100

    Racine Tests good

--- test/RacineTest.java
+++ test/RacineTest.java
@@ -1,4 +1,6 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
@@ -19,5 +21,15 @@ public class RacineTest {
         tab[1] = 9;
         tab[2] = 16;
         assertArrayEquals(Racine.calculer(2,4), tab);
+    }
+
+    @Test
+    public void calculer_a_b_should_return_an_exception_if_a_is_bigger_than_b() {
+        try {
+            Racine.calculer(8,0);
+            fail();
+        }catch (Exception e)  {
+
+        }
     }
 }
